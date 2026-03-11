e29cdad1e32f1ed5132aa3f1f3e3acc52460b834
Thomas-010 <Thomas.rietveld@ziggo.nl>
2026-03-11 09:12:15 +0100

    miniquiz

---
+++ miniquiz/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini Quiz</title>
+</head>
+<body>
+<h2>Mini Quiz<h2>
+
+<form action="check.php" method="post">
+    <label>Wat is de hoofdstad van Frankrijk?<label><br>
+    <input type="text" name="antwoord"><br>
+    <button type="submit">Controleer</button>
+</form> 
+
+</body>
+</html>
